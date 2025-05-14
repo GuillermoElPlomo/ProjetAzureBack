040bfffcde6a7e04c51e0cebea1620f99ff0e207
Johan Ledoux <johan.ledoux25@gmail.com>
2025-05-14 22:51:36 +0200

    v4

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/todos', todosRoutes);
 
-const port = process.env.PORT || 3000;
-const dbUri = process.env.MONGO_URI;
+const port = 3000;
+const dbUri = "";
 
 mongoose.connect(dbUri).then(() => {
   app.listen(port, () => console.log(`Server running on port ${port}`));
